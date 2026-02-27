058c8c87bb292a3eae667704fe9482c1e322c60a
underthestars-zhy <zhuhaoyu0909@icloud.com>
2026-02-27 08:44:17 +0800

    update

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,10 +1,14 @@
 import type { Config } from "drizzle-kit";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+
 export default {
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL ?? "",
+    url: process.env.DATABASE_URL,
   },
 } satisfies Config;
